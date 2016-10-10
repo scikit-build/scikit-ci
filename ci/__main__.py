6682e2f92b3ea25746deaa6aa6aed77e56d20941
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-10-10 01:31:36 -0400

    ci: Add "--version" option

--- ci/__main__.py
+++ ci/__main__.py
@@ -5,6 +5,8 @@ Ci command line tool (enable python -m ci syntax)
 """
 
 import ci
+import os
+import sys
 
 
 def main():
@@ -15,10 +17,17 @@ def main():
 
     import argparse
 
+    version_str = ("This is scikit-ci version %s, imported from %s\n" %
+                   (ci.__version__, os.path.abspath(ci.__file__)))
+
     parser = argparse.ArgumentParser(description=ci.__doc__)
     parser.add_argument(
         "step", type=str, choices=ci.STEPS,
         help="name of the step to execute")
+    parser.add_argument(
+        "--version", action="version",
+        version=version_str,
+        help="display scikit-ci version and import information.")
     args = parser.parse_args()
 
     ci.execute_step(args.step)
